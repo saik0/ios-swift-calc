0d4346eacdc41e9ad17372b9cbe8a23661ef66ba
Joel Pedraza <jpedraza@chewy.com>
2018-05-06 23:38:35 -0400

    wire up number buttons

---
+++ calc/RoundButton.swift
@@ -0,0 +1,27 @@
+//
+//  RoundButton.swift
+//  calc
+//
+//  Created by Joel Pedraza on 5/4/18.
+//  Copyright © 2018 Joel Pedraza. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        recalculateRadius()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        recalculateRadius()
+    }
+    
+    private func recalculateRadius() {
+        layer.cornerRadius = frame.height / 2
+    }
+    
+}
--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calcValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    @IBAction func calcPressed(_ sender: UIButton) {
+        if 0...9 ~= sender.tag {
+            calcValue.text = (calcValue.text ?? "") + String(sender.tag)
+        }
+    }
 }
 
